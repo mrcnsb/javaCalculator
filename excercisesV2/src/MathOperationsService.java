5709478a969952bdd8e3248a4a9ce8dd2ca2d267
mrcnsb <mrcnsb@gmail.com>
2026-02-11 20:11:35 +0100

    calculator ready

--- excercisesV2/src/MathOperationsService.java
+++ excercisesV2/src/MathOperationsService.java
@@ -1,76 +1,81 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class MathOperationsService {
 
     public static void run() {
         Scanner scanner = new Scanner(System.in);
+        List<Character> operations = List.of('+', '-', '*', '/', '%', '^');
 
-        double firstNumber;
-        double secondNumber;
-        char operator;
+        while (true) {
 
-        System.out.println("Podaj liczbę:");
-        if (!scanner.hasNextDouble()) {
-            System.out.println("Nieprawidłowy format");
-            return;
-        }
-        firstNumber = scanner.nextDouble();
+            double firstNumber;
+            double secondNumber;
+            char operator;
 
-        System.out.println("Jakie działanie chcesz wykonać? (+, -, *, /, %, ^)");
-        operator = scanner.next().charAt(0);
+            System.out.println("Podaj liczbę:");
+            if (!scanner.hasNextDouble()) {
+                System.out.println("Nieprawidłowy format");
+                scanner.next();   // usuwa błędny input
+                continue;
+            }
+            firstNumber = scanner.nextDouble();
 
-        System.out.println("Podaj drugą liczbę:");
-        if (!scanner.hasNextDouble()) {
-            System.out.println("Nieprawidłowy format");
-            return;
-        }
-        secondNumber = scanner.nextDouble();
+            System.out.println("Jakie działanie chcesz wykonać? +, -, *, /, %, ^");
+            operator = scanner.next().charAt(0);
 
-        double result;
+            if (!operations.contains(operator)) {
+                System.out.println("Nie ma takiego działania!");
+                continue;
+            }
 
-        switch (operator) {
-            case '+':
-                result = firstNumber + secondNumber;
+            System.out.println("Podaj drugą liczbę:");
+            if (!scanner.hasNextDouble()) {
+                System.out.println("Nieprawidłowy format");
+                scanner.next();
+                continue;
+            }
+            secondNumber = scanner.nextDouble();
+
+            double result;
+
+            switch (operator) {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/':
+                    if (secondNumber == 0) {
+                        System.out.println("Nie można dzielić przez 0");
+                        continue;
+                    }
+                    result = firstNumber / secondNumber;
+                    break;
+                case '%':
+                    result = firstNumber % secondNumber;
+                    break;
+                case '^':
+                    result = Math.pow(firstNumber, secondNumber);
+                    break;
+                default:
+                    continue;
+            }
+
+            System.out.println("Wynik: " + result);
+
+            System.out.println("Czy chcesz wykonać kolejną operację? (t/n)");
+            char decision = scanner.next().charAt(0);
+
+            if (decision == 'n') {
+                System.out.println(((int) result % 2 == 0)
+                        ? "Wynik jest parzysty"
+                        : "Wynik jest nieparzysty");
                 break;
-            case '-':
-                result = firstNumber - secondNumber;
-                break;
-            case '*':
-                result = firstNumber * secondNumber;
-                break;
-            case '/':
-                if (secondNumber == 0) {
-                    System.out.println("Nie można dzielić przez 0");
-                    return;
-                }
-                result = firstNumber / secondNumber;
-                break;
-            case '%':
-                result = firstNumber % secondNumber;
-                break;
-            case '^':
-                result = Math.pow(firstNumber, secondNumber);
-                break;
-            default:
-                System.out.println("Nieznany operator");
-                return;
-        }
-
-        System.out.println("Wynik: " + result);
-
-        System.out.println("Czy chcesz wykonać kolejną operację? (t/n)");
-        char decision = scanner.next().charAt(0);
-
-        if (decision == 't' || decision == 'T') {
-            run();
-        } else {
-
-            int modulo = ((int) result) % 2;
-
-            if (modulo == 0) {
-                System.out.println("Wynik jest parzysty");
-            } else {
-                System.out.println("Wynik jest nieparzysty");
             }
         }
     }
